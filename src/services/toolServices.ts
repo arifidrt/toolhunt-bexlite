1658f5226f24f20e16240838befffc8e186e9213
Arif Indarto <lifealien2020@gmail.com>
2024-08-16 00:25:24 +0700

    Tool Service - Create Data

--- src/services/toolServices.ts
+++ src/services/toolServices.ts
@@ -1,3 +1,4 @@
+import { createId } from "@paralleldrive/cuid2";
 import { client } from "../models/client";
 import { ITool } from "../types/entity";
 
@@ -21,5 +22,45 @@ export const toolServices = {
       )
       .all() as ITool[];
     return allTools;
+  },
+
+  createData: ({ name, description, image, url, category }: Omit<ITool, "id" | "visits" | "verified" | "is_public">) => {
+    const toolId = createId();
+    const analyticId = createId();
+
+    client
+      .query(
+        `
+        INSERT INTO tools (id, name, description, image, url, category) VALUES (?,?,?,?,?,?)
+        `
+      )
+      .run(toolId, name, description, image, url, category);
+    client
+      .query(
+        `
+        INSERT INTO analytics (id, tool_id) VALUES (?,?)
+        `
+      )
+      .run(analyticId, toolId);
+    const currentTool = client
+      .query(
+        `
+            SELECT 
+            t.id AS id,
+            t.name AS name,
+            t.description AS description,
+            t.image AS image,
+            t.url AS url,
+            a.visits AS visits
+            FROM
+            tools t
+            JOIN
+            analytics a on a.tool_id  = t.id
+            WHERE
+            t.id = ?
+            `
+      )
+      .all(toolId) as ITool[];
+    return currentTool[0];
   },
 };
