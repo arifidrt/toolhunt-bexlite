de5ce88f49fc2a92adc04795318d533acab217c7
Arif Indarto <lifealien2020@gmail.com>
2024-08-19 12:25:19 +0700

    Save image to disk

--- src/controllers/appController.tsx
+++ src/controllers/appController.tsx
@@ -3,14 +3,17 @@ import { toolServices } from "../services/toolServices";
 import { ITool } from "../types/entity";
 
 export const appController = {
-  handleCreateTool: ({ body }: Context) => {
+  handleCreateTool: async ({ body }: Context) => {
     const { name, image, url, category, description } = body as ITool;
 
     if (!name || !image || !url || !category || !description) {
       return <div class="msg-error">All fields are required!</div>;
     }
     try {
-      toolServices.createData({ name, description, image: (image as Blob).name, url, category });
+      const tool = toolServices.createData({ name, description, image: (image as Blob).name, url, category });
+
+      await Bun.write(`./public/images/${tool.id}/${tool.image}`, image as Blob);
+
       return new Response(null, {
         headers: {
           "HX-Redirect": "/",
